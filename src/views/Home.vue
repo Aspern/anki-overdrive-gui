32a6a32bffb7526f09b1edf85ea1de370cfde2a0
xgxtadw <adrian.weber@msg.group>
2018-10-26 09:06:18 +0200

    Initial commit with vue typescript project

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <start-page msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import StartPage from '@/components/StartPage.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+      StartPage,
+  },
+})
+export default class Home extends Vue {}
+</script>
